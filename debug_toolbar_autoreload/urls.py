887bf66893e57df3e6c5c7c558f309de2a02c5a5
Adrián López Calvo <adrianlopezcalvo@gmail.com>
2013-08-04 21:17:08 +0200

    url import replaced with new path + fallback

--- debug_toolbar_autoreload/urls.py
+++ debug_toolbar_autoreload/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError: # django < 1.4
+    from django.conf.urls.defaults import patterns, url
 from debug_toolbar.urls import _PREFIX
 
 
